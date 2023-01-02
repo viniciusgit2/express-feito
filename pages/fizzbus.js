bb6d04cf41b6851fb55a69c6b45fde50cda73845
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2023-01-02 08:32:22 -0300

    com webpack.config

---
+++ pages/fizzbus.js
@@ -0,0 +1,31 @@
+export default function  fizbuss (){
+     const contador=3
+console.log(contador)
+if(contador===3){
+console.log("fizbuszz")
+}
+else{
+    console.log("erro")
+
+}
+}
+   
+   
+   
+   
+   
+   
+   
+
+   
+    
+    
+    
+    
+    
+
+
+
+
+
+
