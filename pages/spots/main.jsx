5e868f38b08a2e7f4cac6c0edc9131a5111a18b3
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2023-01-05 08:18:56 -0300

    adicionando o chai

--- pages/spots/main.jsx
+++ pages/spots/main.jsx
@@ -1,13 +1,16 @@
+import { type } from "os";
+
+export const search=(query,type)
+fetch("https://api.spotify.com/v1/search?q=incubus")
+console.log(fetch)
 
 
 
 
 
-export default function main(){
-    const element = <a href="https://open.spotify.com"> spotify </a>;
-return(element)
 
 
 
 
-}
+
+
--- pages/spots/testes/main.spec.js
+++ pages/spots/testes/main.spec.js
@@ -17,5 +17,21 @@ describe("generice search",()=>{
   it("should call back function",()=>{ const fetchedstub =sinonstub(global,'fetch') 
     const artista=search()
   expect(fetchedstub).to.have.been.calledonce})
-})
+},)
+describe("generice search",()=>{
+  it("should call back function",()=>{ const fetchedstub =sinonstub(global,'fetch') 
+    const artista=search("incubs,artista")
+
+  expect(fetchedstub).to.have.been.calledwitch("http://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6")})
+},)
+
+describe("generice search",()=>{
+  it("should call back function",()=>{ const fetchedstub =sinonstub(global,'fetch') 
+    const album=search("incubs,album")
+
+  expect(fetchedstub).to.have.been.calledwitch("spotify:track:6rqhFgbbKwnb9MLmUQDhG6")})
+},)
+
+
 //é pra dar ero nao está com nem metade criado ainda se nao der erro melhor
+// depois trocar o endreço
