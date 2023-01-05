d2917ff1236fe24fd28b268dc729847d316e830b
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2023-01-05 09:54:48 -0300

    mais testes

--- pages/spots/main.jsx
+++ pages/spots/main.jsx
@@ -1,12 +1,14 @@
 import { type } from "os";
 
 export const search=(query,type)
-fetch("https://api.spotify.com/v1/search?q=incubus")
+fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
+.then(data=>data.json)
 console.log(fetch)
 
-
-
-
+const searchartista=()=>{}
+const searchalbum=()=>{}
+const searchtrack=()=>{}
+const searchplaylist=()=>{}
 
 
 
