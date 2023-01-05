a4f37e40cc776b481701a9af633f0d5bfc00e5be
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2023-01-05 09:24:36 -0300

    tem linha desnecessaria depois revisar

--- pages/spots/testes/main.spec.js
+++ pages/spots/testes/main.spec.js
@@ -2,7 +2,7 @@ import chai, {expect} from "chai"
 import sinon from "sinon"
 import sinonchai from "sinon-chai"
 import sinonstubpromisse from "sinon-stub-promise"
-import { describe, it } from "node:test"
+import { afterEach, beforeEach, describe, it } from "node:test"
 global.fetch =require ("node-fetch")
 chai.use(sinonchai)
 sinonstubpromisse(sinon)
@@ -14,15 +14,19 @@ it("should exist the search method",()=>{ expect(search).to.existe;})
 
 )
 describe("generice search",()=>{
+  beforeEach(()=>{  let fetchedstub})
+
   it("should call back function",()=>{ const fetchedstub =sinonstub(global,'fetch') 
+  afterEach(()=>{fetchedstub.restore})
     const artista=search()
   expect(fetchedstub).to.have.been.calledonce})
 },)
+
 describe("generice search",()=>{
   it("should call back function",()=>{ const fetchedstub =sinonstub(global,'fetch') 
     const artista=search("incubs,artista")
 
-  expect(fetchedstub).to.have.been.calledwitch("http://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6")})
+  expect(fetchedstub).to.have.been.calledwitch("https://api.spotify.com/v1/search?q=incubus")})
 },)
 
 describe("generice search",()=>{
@@ -33,5 +37,5 @@ describe("generice search",()=>{
 },)
 
 
-//é pra dar ero nao está com nem metade criado ainda se nao der erro melhor
+//é pra dar erro nao está com nem metade criado ainda se nao der erro melhor
 // depois trocar o endreço
